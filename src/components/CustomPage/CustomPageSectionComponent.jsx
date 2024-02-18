ba9c60792bdc6de03a45816706a30d2e03fb7be4
Sitta Sahathum <sittasahathum@gmail.com>
2024-02-18 23:30:30 +0700

    add button style of compoment Slider

--- src/components/CustomPage/CustomPageSectionComponent.jsx
+++ src/components/CustomPage/CustomPageSectionComponent.jsx
@@ -7,6 +7,26 @@ import dataset_model from './DatasetModel'
 
 export const SliderComponent = () => {
     const sliderRef = useRef(null); 
+    const [isSliderOpen, setIsSliderOpen] = useState(true);
+    const toggleSlider = () => {
+      setIsSliderOpen(!isSliderOpen);
+    };
+    const getToggleIcon = () => {
+      return isSliderOpen ? (
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
+          </svg>
+        </span> 
+      ) : (
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+          </svg>
+        </span> 
+      );
+    };
+
 
     const cardTitles = ["Model", "Wings", "Battery","Legs"];
   
@@ -22,7 +42,7 @@ export const SliderComponent = () => {
   
     const settings = {
       infinite: true,
-      speed: 0,
+      speed: 5,
       slidesToShow: 1,
       slidesToScroll: 1,
       draggable: false, 
@@ -39,51 +59,75 @@ export const SliderComponent = () => {
     const slideNext = () => {
       sliderRef.current.slickNext();
     };
+    
     return (
     <>
-      <div className='all-slider-container'>
-        <div className="arrow-container">
-        </div>
-        <Slider ref={sliderRef} {...settings}>
-          {cardTitles.map((title, index) => (
+        <div className="slide-top">
+          <div className="box-a-slide">
+          <div className={`all-slider-container ${isSliderOpen ? 'open' : 'closed'}`}>
 
-            <div key={index} className="full-page-card">
-              <div className="title-card-slider">
-                <h1>{title}</h1>
-                <div className='Arrow-all'>
-                <button onClick={slidePrev} className="arrow-button" id='arrow-btn-left'>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-                        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
-                      </svg>
-                </button>
-                <button onClick={slideNext} className="arrow-button" id='arrow-btn-right'>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-                      </svg>
-                </button>
+            <button onClick={toggleSlider} className="toggle-button">
+              {getToggleIcon()}
+            </button>
+          </div>
+          </div>
+
+          <div className="box-b-slide">
+
+            <div className={`all-slider-container ${isSliderOpen ? 'open' : 'closed'}`}>
+              
+              <div className="slider-content">
+
+              
+                <div className="arrow-container">
+                </div>
+                <Slider ref={sliderRef} {...settings}>
+                  {cardTitles.map((title, index) => (
+
+                    <div key={index} className="full-page-card">
+                      <div className="title-card-slider">
+                        <h1>{title}</h1>
+                        <div className='Arrow-all'>
+                        <button onClick={slidePrev} className="arrow-button" id='arrow-btn-left'>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+                              </svg>
+                        </button>
+                        <button onClick={slideNext} className="arrow-button" id='arrow-btn-right'>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                              </svg>
+                        </button>
+                        </div>
+                      </div>
+                      <nav className="navbar">
+                        <ul className='Slider-container'>
+                          {cardTitles.map((navTitle, navIndex) => (
+                            <li onClick={() => goToSlide(navIndex)} key={navIndex} className={index === navIndex ? 'active' : ''}>
+                              <a id='name-title-click' href="#">
+                                {navTitle}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                        <div className="spaceInside" >
+                          {comsTitles[index]}
+                        </div>
+                      </nav>
+                    </div>
+                  ))}
+                </Slider>
+                <div className="arrow-container">
+
+                </div>
                 </div>
               </div>
-              <nav className="navbar">
-                <ul className='Slider-container'>
-                  {cardTitles.map((navTitle, navIndex) => (
-                    <li onClick={() => goToSlide(navIndex)} key={navIndex} className={index === navIndex ? 'active' : ''}>
-                      <a id='name-title-click' href="#">
-                        {navTitle}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-                <div className="spaceInside" >
-                  {comsTitles[index]}
-                </div>
-              </nav>
-            </div>
-          ))}
-        </Slider>
-        <div className="arrow-container">
 
+            
+          </div>
         </div>
-      </div>
+          
+          
 
     </>
     )
@@ -112,19 +156,29 @@ const BoxComponent = ({ numOfBoxes , imageProps, namesProp, pricesProp }) => {
   };
 
   const createBox = (key) => (
-    <div key={key} style={boxStyle}>
+    <div key={key} style={boxStyle} className='BoxComponent'>
       <div style={{ boxShadow: '0px 4px rgba(0, 0, 0, 0.2)', marginTop: '-1vh', width: '10.7vw', height: '78%', backgroundColor: '#E2E3E3', borderRadius: '1.2vw' }}>
-        <div className="InnerBox">{imageProps[key]}</div>
+        <div className="InnerBox" >
+          <img src={imageProps[key]} alt="" />
+        </div>
       </div>
       <div style={{ textAlign: 'center', fontSize: '0.8em' }}>
         <div className="Description-Boxes">
           <h4>{namesProp[key]}</h4>
           <div className="description-under">
             <p>{pricesProp[key]}</p>
+            <p className="description-info">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
+            </svg>
+            </p>
           </div>
         </div>
       </div>
+    
+
     </div>
+    
   );
   
   const createBoxes = (count, start) => Array.from({ length: count }, (_, index) => createBox(start + index));
--- src/components/CustomPage/DatasetModel.jsx
+++ src/components/CustomPage/DatasetModel.jsx
@@ -2,7 +2,11 @@ const dataset_model = [
     {
       // title : "Model",
       numOfBoxes: 4,
-      imageProps: ["ModelA","ModelB", "ModelC", "ModelD"],
+      imageProps: [
+      "https://images.unsplash.com/photo-1690885732016-f44e973ed3bf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1699959634881-16f34059a78f?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+      "https://images.unsplash.com/photo-1690885744537-1ff88d3ff9ad?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+      "https://images.unsplash.com/photo-1708169967312-889c71da622b?q=80&w=1744&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
       namesProp: ["a","b","c", "d"],
       pricesProp: [1,2, 3, 4],
       
